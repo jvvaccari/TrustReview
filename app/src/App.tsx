c77ff2edf6ef460c6c0af49f05a3ca9ff4fa6250
jvvaccari <jvvaccari1@gmail.com>
2025-09-22 02:02:42 -0300

    feat: add ProductTitle component with background image and styled typography

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,7 @@ import Search from './components/Search'
 import { defaultTheme } from './theme/default'
 import ProductCard from './components/ProductCard'
 import img_test from './assets/img_test.jpg'
+import ProductTitle from './components/ProductTitle'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <ThemeProvider theme={defaultTheme}>
       <Stack spacing={4} sx={{ width: '100vw', height: '100vh', bgcolor: 'background.default', alignItems: 'center', justifyContent: 'center' }}>
         <Box width={300}>
+          <ProductTitle />
           <Search value={''} onChange={() => {}} />
         </Box>
         <ProductCard productData={{ productId: '1', imageUrl: img_test }} />
